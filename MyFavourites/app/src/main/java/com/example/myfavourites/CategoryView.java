f8a29e791bed2bfe39406914ebd71bf7a2d2d32c
Vicky <vickythetechguy@gmail.com>
2019-07-01 12:45:46 +0530

    ProjectDocumentation

--- MyFavourites/app/src/main/java/com/example/myfavourites/CategoryView.java
+++ MyFavourites/app/src/main/java/com/example/myfavourites/CategoryView.java
@@ -11,8 +11,6 @@ import java.util.ArrayList;
 
 public class CategoryView extends AppCompatActivity {
 
-    ArrayList<String> categorylist=new ArrayList<>();
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,21 +18,7 @@ public class CategoryView extends AppCompatActivity {
 
         Intent intent=getIntent();
 
-        categorylist.add("VIDEO");
-        categorylist.add("AUDIO");
-        categorylist.add("NOTEPAD");
-        categorylist.add("PAINT");
-        categorylist.add("CAMERA");
-        categorylist.add("OTHERS");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                R.layout.list_item,
-                R.id.textView,
-                categorylist);
-
-        ListView listView = findViewById(R.id.listView);
-
-        listView.setAdapter(arrayAdapter);
 
     }
 }
